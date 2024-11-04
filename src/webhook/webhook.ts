202d0a2ac2c8e54c0b07078559e63cbdfd789bcc
Juan Pablo <juansh821@gmail.com>
2024-11-04 15:34:40 -0500

    Add functions to webhook

--- src/webhook/webhook.ts
+++ src/webhook/webhook.ts
@@ -19,7 +19,48 @@ export const webhookRouter = new Elysia({ prefix: '/webhook' })
         throw new Error('Signature mismatch');
     }
 
-    console.log(body);
+    const bodyString = bodyBuffer.toString('utf8'); // Convert Buffer to string
+    const bodyJson = JSON.parse(bodyString); // Parse JSON from string
+
+    console.log(bodyJson); // Logs the parsed JSON object
+
+    // Example: Access specific data from the parsed JSON
+    const userId = bodyJson?.data?.meta?.custom_data?.userId;
+    const date = bodyJson?.data?.attributes?.trial_ends_at || bodyJson?.data?.attributes?.ends_at;
+
+    if (!userId) {
+        throw new Error('User ID not found in payload');
+    }
+
+    // Process based on event name
+    if (bodyJson?.meta?.event_name === 'subscription_created') {
+        const user = await prisma.prospectUser.findUnique({
+            where: { id: Number(userId) },
+        });
+        await prisma.prospectUser.update({
+            wher: {
+                email: user?.email
+            }, 
+            data: {
+                subscriptionId: bodyJson?.data?.id,
+            }
+        })
+        if (user) {
+            createUser({ email: user.email, nick: user.nick, password: user.password, date });
+        }
+    } else if (bodyJson?.meta?.event_name === 'subscription_updated') {
+        const user = await prisma.prospectUser.findUnique({
+            where: { id: Number(userId) },
+        });
+
+        if (user) {
+            await prisma.user.update({
+                where: { email: user.email },
+                data: { pro: new Date(date) },
+            });
+        }
+    }
+
     return "ok";
 })
 .onError(({ error }) => {
